01ceaa2960358f969ef8cc56cf01c0fa76a8229c
Satyam <sm2003satyam@gmail.com>
2025-05-01 18:21:14 +0530

    refactor DesktopMainLayout to integrate theme-based styling and improve sidebar handling

--- youtube/src/components/layout/desktop/DesktopMainLayout.jsx
+++ youtube/src/components/layout/desktop/DesktopMainLayout.jsx
@@ -1,40 +1,46 @@
 import DesktopNavbar from "../../navbar/desktop/DesktopNavbar.jsx"
-import { useLocation, Outlet } from "react-router-dom"
-import CollapsedSidebarItem from "../../sidebars/web/sidebarItems/CollapsedSidebarItem.jsx"
-import MobileSidebar from "../../sidebars/mobile/MobileSidebar.jsx"
-import SubscriptionsIcon from '@mui/icons-material/Subscriptions'
-import SubscriptionsOutlinedIcon from '@mui/icons-material/SubscriptionsOutlined'
-import AccountCircleIcon from '@mui/icons-material/AccountCircle'
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined'
+import { Outlet } from "react-router-dom"
 import ExpandedSidebar from "../../sidebars/web/ExpandedSidebar.jsx"
-import getTheme from "../../../services/theme/getTheme.js"
-import { shouldShowComponent } from "../../../utils/shouldShowComponent.js"
-import { useSelector } from "react-redux"
-import YoutubeNavbar from "../../navbar/YoutubeNavbar.jsx"
 import CollapsedSidebar from "../../sidebars/web/CollapsedSidebar.jsx"
-import { COLLAPSED_SIDEBAR, NAVBAR } from "../../../constants/styles/constants.js"
+import { COLLAPSED_SIDEBAR, EXPANDED_SIDEBAR, NAVBAR } from "../../../constants/styles/constants.js"
+import { useTheme } from "../../../contexts/ThemeContext.jsx"
 
 const DesktopMainLayout = () => {
-    const youtubeSidebarState = useSelector((state) => state.sidebar.isExpanded)
-
+    const { theme } = useTheme()
+    const themeClass = theme === "light" ? 'bg-white text-black' : 'bg-black text-white'
     return (
-        <div className={`flex h-screen`}>
-            <div 
-            style={{ width: COLLAPSED_SIDEBAR.width }}
-            className="fixed">
-                <CollapsedSidebar />
-            </div>
-            <div 
-            style={{ width: NAVBAR.width, left: COLLAPSED_SIDEBAR.width }}
-            className="relative">
+        <div className={`${theme === "light" ? 'bg-white text-black' : 'bg-black text-white'} relative flex h-screen`}>
+            {
+                isSidebarExpanded === true
+                ? (
+                    <div 
+                    style={{ width: EXPANDED_SIDEBAR.width }}
+                    className={`${themeClass} fixed z-20`}>
+                        <ExpandedSidebar />
+                    </div>
+                )
+                : (
+                    <div 
+                    style={{ width: COLLAPSED_SIDEBAR.width }}
+                    className={`${themeClass} fixed z-20`}>
+                        <CollapsedSidebar />
+                    </div>
+                )
+            }
+
+            <div className="w-screen relative">
               <div 
               style={{ width: NAVBAR.width, left: COLLAPSED_SIDEBAR.width }}
-              className="fixed z-20">
+              className={`${themeClass} fixed z-20`}>
                   <DesktopNavbar />
               </div>
               <div 
-              style={{ top: NAVBAR.height }}
-              className="relative w-full">
+              style={{ 
+                width: `calc(100% - ${isSidebarExpanded ? EXPANDED_SIDEBAR.width : COLLAPSED_SIDEBAR.width})`,
+                left: isSidebarExpanded ? EXPANDED_SIDEBAR.width : COLLAPSED_SIDEBAR.width,
+                top: NAVBAR.height
+            }}
+              className={`${themeClass} relative`}>
                   <Outlet />
               </div>
             </div>
