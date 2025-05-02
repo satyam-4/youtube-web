ec569fe1230e621b57a9982b63703289148f64fc
Satyam <sm2003satyam@gmail.com>
2025-05-02 14:06:49 +0530

    Refactor MenuIcon to use ThemeContext for theme-based styling

--- youtube/src/components/icons/MenuIcon.jsx
+++ youtube/src/components/icons/MenuIcon.jsx
@@ -1,10 +1,12 @@
-import getTheme from "../../services/theme/getTheme.js"
+import { useTheme } from "@contexts/ThemeContext"
 
-const theme = getTheme()
+export const MenuIcon = () => {
+    const { theme } = useTheme()
 
-export const MenuIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" 
-    className={`${theme === "light" ? 'text-lightDark' : 'text-lightWhite'} size-[1.8rem]`}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-    </svg>
-)
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" 
+        className={`${theme === "light" ? 'text-lightDark' : 'text-lightWhite'} size-[1.8rem]`}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+    )
+}
