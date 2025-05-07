ebc100ae7eaebf3dee71556cc6d39c9a95f2bc8c
Satyam <sm2003satyam@gmail.com>
2025-05-07 14:09:49 +0530

    Refactor CollapsedSidebar to use SidebarContext for state management and simplify rendering of sidebar items

--- youtube/src/components/sidebars/web/CollapsedSidebar.jsx
+++ youtube/src/components/sidebars/web/CollapsedSidebar.jsx
@@ -1,19 +1,19 @@
 import React from "react"
 import { NavLink } from "react-router-dom"
 import { COLLAPSED_SIDEBAR_CONFIG } from "../../../config/desktop/sidebarConfig/CollapsedSidebarConfig"
-import getTheme from "../../../services/theme/getTheme.js"
 import { COLLAPSED_SIDEBAR } from "../../../constants/styles/constants.js"
-import { useDispatch } from "react-redux"
-import { toggleYoutubeSidebar } from "../../../features/sidebar/sidebarSlice"
+import { useSidebar } from "@contexts/SidebarContext.jsx"
+import { useTheme } from "@contexts/ThemeContext.jsx"
+import CollapsedSidebarItem from "./sidebarItems/CollapsedSidebarItem.jsx"
 
 const CollapsedSidebar = () => {
-    const dispatch = useDispatch()
-    const theme = getTheme()
+    const { theme } = useTheme()
+    const { toggleSidebarState } = useSidebar()
 
     const handleMenuClick = (action) => {
         console.log("clicked button")
         if(action === "TOGGLE_SIDEBAR") {
-            dispatch(toggleYoutubeSidebar())
+            toggleSidebarState()
         }
     }
 
@@ -21,7 +21,7 @@ const CollapsedSidebar = () => {
         COLLAPSED_SIDEBAR_CONFIG.map((sidebarItem) => (
             sidebarItem.id === "menu"
             ? (
-                <div className="h-[65px] bg-dark flex justify-center items-center">
+                <div className="h-[65px] flex justify-center items-center">
                     <button
                     onClick={() => handleMenuClick(sidebarItem.action)}>
                         {
@@ -31,33 +31,11 @@ const CollapsedSidebar = () => {
                 </div>
             )
             : (
-                <NavLink
-                className={({ isActive }) => 
-                    `py-5 rounded-md flex flex-col items-center gap-2
-                    ${theme === "light" ? 
-                        (isActive ? 'bg-[#ff000025] hover:bg-[#ff00003c]' : 'hover:bg-[#ff000025]') : 
-                        (isActive ? 'bg-[#bababa21] hover:bg-[#bababa38]' : 'hover:bg-[#8080802a]')
-                    }`  
-                }
-                to={sidebarItem.link}>
-                    {
-                        ({ isActive }) => (
-                            <>
-
-                                    <div 
-                                    className="flex justify-center">
-                                        {
-                                            isActive ? sidebarItem.activeIcon() : sidebarItem.inactiveIcon()
-                                        }
-                                    </div>
-
-                                <span className="text-[0.6rem]">
-                                    { sidebarItem.label }
-                                </span>
-                            </>
-                        )
-                    }
-                </NavLink>
+                <CollapsedSidebarItem
+                to={sidebarItem.link}
+                activeIcon={sidebarItem.activeIcon()}
+                inactiveIcon={sidebarItem.inactiveIcon()}
+                label={sidebarItem.label} />
             )
         ))
     )
