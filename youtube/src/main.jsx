3d7863520ea7efe573ea9660a87e19cb8c103525
Satyam <sm2003satyam@gmail.com>
2025-05-19 18:50:53 +0530

    feat: add NotFoundRoutes to handle 404 page rendering in router setup

--- youtube/src/main.jsx
+++ youtube/src/main.jsx
@@ -3,16 +3,21 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import { Provider } from 'react-redux'
 import { AuthRoutes } from "./routes/AuthRoutes.jsx"
 import { DefaultRoutes } from "./routes/DefaultRoutes.jsx"
+import { NotFoundRoutes } from "./routes/NotFoundRoutes.jsx"
 import store from "./app/store.js"
 import "./index.css"
+import { ThemeProvider } from "@contexts/ThemeContext.jsx"
 
 const router = createBrowserRouter([
   DefaultRoutes,
-  AuthRoutes
+  AuthRoutes,
+  NotFoundRoutes
 ])
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </Provider>
 )
